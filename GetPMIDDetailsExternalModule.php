72f48ef041c098e5be9b8f42bdd5fd188e5f2058
knil-maloon <linkedfrends@gmail.com>
2023-11-21 14:29:17 -0600

    First Upload

---
+++ GetPMIDDetailsExternalModule.php
@@ -0,0 +1,82 @@
+<?php
+namespace Vanderbilt\GetPMIDDetailsExternalModule;
+
+use ExternalModules\AbstractExternalModule;
+use ExternalModules\ExternalModules;
+
+
+class GetPMIDDetailsExternalModule extends AbstractExternalModule
+{
+
+    function redcap_every_page_before_render($project_id){
+        $instrument = $this->getProjectSetting('instrument-name');
+        if($_REQUEST['page'] == $instrument) {
+            $record = (int)$_REQUEST['id'];
+            self:$this->getPMIDLink($project_id,$record);
+        }
+    }
+
+    function redcap_survey_page($project_id,$record){
+        self:$this->getPMIDLink($project_id,$record);
+    }
+
+    function getPMIDLink($project_id,$record){
+        echo '<script type="text/javascript" src="'.$this->getUrl('js/jquery-3.3.1.min.js').'"></script>';
+        echo '<script>
+                function getLink(){
+                    var value = document.getElementsByName("output_pmid")[0].value;
+                    var url = '.json_encode($this->getUrl('getPMIDUrl.php')).';
+                    var pid = '.json_encode($project_id).';
+                    var record = '.json_encode($record).';
+                    var redcap_csrf_token = '.json_encode($this->getCSRFToken()).';
+                    if(value == ""){
+                        alert("You need a PMID value to retrieve the data.")
+                    }else{
+                        document.querySelector("[name=\'output_pmid_btn\']").innerHTML = \'<i class="fa fa-spinner fa-spin"></i> Loading...\';
+                        $.ajax({
+                            type: "GET",
+                            url: url,
+                            data: "&pid="+pid+"&record="+record+"&value="+value+"&redcap_csrf_token="+redcap_csrf_token,
+                            error: function (xhr, status, error) {
+                                alert(xhr.responseText);
+                            },
+                            success: function (result) {
+                                jsonAjax = jQuery.parseJSON(result);
+                                
+                                document.querySelector("[name=\'output_pmid_btn\']").innerHTML = \'<i class="fa-solid fa-download"></i>  Get PMID Details\';
+                      
+                                if(jsonAjax.message == "success"){
+                                    window.location.reload();
+                                }else{
+                                    let message = document.querySelectorAll("[name=\'output_pmid_message\']");
+                                    message.forEach(element => {
+                                        element.remove();
+                                    });
+                                    
+                                    const el = document.querySelector("[name=\'output_pmid_btn\']");
+                                    var btn = \'<div class="alert alert-danger" name="output_pmid_message" style="margin-top:10px">\' +
+                                             jsonAjax.message+
+                                            \'</div>\';
+                                    el.insertAdjacentHTML(\'afterend\', btn)
+                                }
+                            }
+                        });
+                    }
+                }
+                document.addEventListener("DOMContentLoaded",  function () { 
+                    const el = document.querySelector("[name=\'output_pmid\']");
+                    var btn = \'<div style="margin-top: 10px;float: right;">\' +
+                                 \'<button type="button" name="output_pmid_btn" class="btn btn-xs fs14 btn-rcgreen" onclick="getLink()">\'+
+                                    \'<i class="fa-solid fa-download"></i>\'+
+                                    \' Get PMID Details\'+
+                                 \'</button>\' +
+                            \'</div>\';
+                    el.insertAdjacentHTML(\'afterend\', btn)
+                });
+        </script>';
+    }
+}
+
+
+
+?>
---
+++ getPMIDUrl.php
@@ -0,0 +1,64 @@
+<?php
+namespace Vanderbilt\GetPMIDDetailsExternalModule;
+
+$record = (int)$_REQUEST['record'];
+$pmid = (int)$_REQUEST['value'];
+$pid = (int)$_REQUEST['pid'];
+
+$api_key = $module->getProjectSetting('api-key');
+$instrument = $module->getProjectSetting('instrument-name');
+$url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=pubmed&retmode=json";
+if(isset($api_key)){
+    $url .= "&api_key=".$api_key;
+}
+if(isset($pmid)){
+    $url .= "&id=".$pmid;
+}
+
+#Get the JSON
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_URL, $url);
+$result = curl_exec($ch);
+curl_close($ch);
+
+$pmid_data = json_decode($result,true);
+if(empty($pmid_data["error"])) {
+    $authors = "";
+    foreach ($pmid_data['result'][$pmid]['authors'] as $data) {
+        $authors .= $data["name"] . ", ";
+    }
+    $pmcid = "";
+    foreach ($pmid_data['result'][$pmid]['articleids'] as $label => $value) {
+        if ($value['idtype'] == "pmc") {
+            $pmcid = $value['value'];
+            break;
+        }
+    }
+    $authors = rtrim($authors, ", ");
+    $array = array(array(
+        'record_id' => $record,
+        'output_title' => $pmid_data['result'][$pmid]['title'],
+        'output_year' => $pmid_data['result'][$pmid]['pubdate'],
+        'output_authors' => $authors,
+        'output_venue' => $pmid_data['result'][$pmid]['source'],
+        'output_citation' => $pmid_data['result'][$pmid]['source'] . ", " . $pmid_data['result'][$pmid]['epubdate'],
+        'output_pmid' => $pmid,
+        'output_pmcid' => $pmcid,
+        'output_url' => "https://pubmed.ncbi.nlm.nih.gov/" . $pmid,
+        $instrument.'_complete' => 2
+    ));
+    $jsonRM = json_encode($array);
+    $results = \Records::saveData($pid, 'json', $jsonRM, 'overwrite', 'YMD', 'flat', '', true, true, true, false, true, array(), true, false);
+
+    if(empty($results['errors'])){
+        echo json_encode(array("message"=>"success"));
+    }else {
+        echo json_encode(array("message" => "Something went wrong when saving the data"));
+    }
+}else{
+    echo json_encode(array("message" => "There are 0 results for PIMD #".$pmid));
+}
+
+?>
